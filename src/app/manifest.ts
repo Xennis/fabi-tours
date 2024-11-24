fdb09c5f2957582ba7f9f80afa6b163e9ab2db2d
Xennis <code@xennis.org>
2024-11-24 11:32:03 +0100

    Allow static site export

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,6 +2,9 @@ import type { MetadataRoute } from "next"
 
 import { brandColor } from "@/content/config"
 
+// static site
+export const dynamic = "force-static"
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "FabiTours",
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,9 @@ import { type MetadataRoute } from "next"
 
 import { host } from "@/content/config"
 
+// static site
+export const dynamic = "force-static"
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
