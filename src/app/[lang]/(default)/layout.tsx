26c5360aeaea3e56f31f0123b93d09c692092690
Xennis <code@xennis.org>
2025-01-28 11:32:09 +0100

    Upgrade TailwindCSS to v4

--- src/app/[lang]/(default)/layout.tsx
+++ src/app/[lang]/(default)/layout.tsx
@@ -1,3 +1,3 @@
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
-  return <main className="mx-auto w-full max-w-screen-xl px-3 py-10">{children}</main>
+  return <main className="mx-auto w-full max-w-(--breakpoint-xl) px-3 py-10">{children}</main>
 }
