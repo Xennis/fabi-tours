32beb1976e38e05e0134479e31b2bb4b5e51b8ec
Xennis <code@xennis.org>
2025-09-07 20:49:09 +0200

    chore: Update @notionhq/client to v5

--- src/lib/cms/fetchers.ts
+++ src/lib/cms/fetchers.ts
@@ -2,11 +2,12 @@ import { unstable_cache } from "next/cache"
 import type { AlternateURLs } from "next/dist/lib/metadata/types/alternative-urls-types"
 import { fetchDatabasePages } from "@xennis/react-notion-cms-fetch"
 import { fetchBlocksChildren } from "@xennis/react-notion-cms-render"
+import { Client } from "@notionhq/client"
+import { isFullDatabase } from "@notionhq/client/build/src/helpers"
 
 import { processPlace } from "@/lib/cms/places"
 import { processPages } from "@/lib/cms/pages"
 import { i18n } from "@/content/i18n"
-import { Client } from "@notionhq/client"
 
 const notionClient = new Client({
   auth: process.env.NOTION_ACCESS_TOKEN,
@@ -17,7 +18,7 @@ export async function getCachedPlaces() {
   return await unstable_cache(
     async () => {
       return fetchDatabasePages(notionClient, processPlace, {
-        database_id: process.env.NOTION_PLACES_DB_ID!,
+        data_source_id: await mustGetFirstDataSourceId(process.env.NOTION_PLACES_DB_ID!),
         page_size: 50,
       })
     },
@@ -33,7 +34,7 @@ export async function getCachedPages() {
     async () => {
       console.debug(`fetchDatabasePages ${process.env.NOTION_PAGES_DB_ID}`)
       const pages = await fetchDatabasePages(notionClient, processPages, {
-        database_id: process.env.NOTION_PAGES_DB_ID!,
+        data_source_id: await mustGetFirstDataSourceId(process.env.NOTION_PAGES_DB_ID!),
         page_size: 50,
       })
       return pages.map((p) => {
@@ -78,4 +79,13 @@ export async function getCachedPageContent(blockId: string) {
       revalidate: 5 * 60,
     },
   )()
+}
+
+// Workaround for @notionhq/client v5.
+const mustGetFirstDataSourceId = async (database_id: string): Promise<string> => {
+  const db = await notionClient.databases.retrieve({ database_id: database_id })
+  if (isFullDatabase(db) && db.data_sources.length > 0) {
+    return db.data_sources[0].id
+  }
+  throw Error("No data source found in database " + database_id)
 }
