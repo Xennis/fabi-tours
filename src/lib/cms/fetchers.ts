c01e3506eee5b9de3cfe28c7afde399fa99e7f7d
Xennis <code@xennis.org>
2025-09-07 21:01:28 +0200

    chore: Ensure fetchers run on server due to env used

--- src/lib/cms/fetchers.ts
+++ src/lib/cms/fetchers.ts
@@ -1,3 +1,5 @@
+"use server"
+
 import { unstable_cache } from "next/cache"
 import type { AlternateURLs } from "next/dist/lib/metadata/types/alternative-urls-types"
 import { fetchDatabasePages } from "@xennis/react-notion-cms-fetch"
